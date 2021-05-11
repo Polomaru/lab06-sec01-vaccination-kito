d47aef7babc389ca75fdbc4353780a32a735aa9a
jeanpaulHT <jean.huby@utec.edu.pe>
2021-05-11 07:36:07 -0500

    spring initialized

---
+++ src/test/java/lab06_grupo_kito/lab06/Lab06ApplicationTests.java
@@ -0,0 +1,13 @@
+package lab06_grupo_kito.lab06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

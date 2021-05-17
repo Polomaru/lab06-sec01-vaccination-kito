1bf8cd23161db35eb930ea82556018309c354d02
unknown <paul.rios@utec.edu.pe>
2021-05-16 22:44:57 -0500

    Intento 1 en tests

--- src/test/java/lab06_grupo_kito/lab06/Lab06ApplicationTests.java
+++ src/test/java/lab06_grupo_kito/lab06/Lab06ApplicationTests.java
@@ -1,13 +1,30 @@
 package lab06_grupo_kito.lab06;
+import cs.software.demo.DemoApplication;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.lang.String;
+import java.util.logging.Logger;
 
+import controller.CitizenController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.logging.Logger;
+
+
+@SpringBootTest(classes={cs.software.demo.DemoApplication.class})
 class Lab06ApplicationTests {
+    static final Logger logger = Logger.getLogger(DemoApplication.class.getName());
+
+     @Autowired
+     private CitizenController controller;
+
 
     @Test
-    void contextLoads() {
+    public void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
     }
-
+//    @Test(threadPoolSize = 80)
+//    public void testCase3()
+//    { }
 }
